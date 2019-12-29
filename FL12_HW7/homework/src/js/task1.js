bcc9310bbba2ef3e6224e18225d8dc5448cf96fd
Nataliia <opruskon24@gmail.com>
2019-12-29 13:23:05 +0200

    HW7

---
+++ FL12_HW7/homework/src/js/task1.js
@@ -0,0 +1,41 @@
+let email = prompt('Enter your email', '');
+
+if (email === '' || email === null) {
+    alert('Canceled');
+} else if (email.length < 5) {
+    alert("I don't know any emails having name length less than 5 symbols");
+} else if (email !== 'user@gmail.com' && email !== 'admin@gmail.com') {
+    alert("I don’t know you")
+} else {
+    let password = prompt('Enter your password', '');
+    if (password ==='' || password === null) {
+        alert('Canceled')
+    } else if (email === 'user@gmail.com' && password !== 'UserPass' || email === 'admin@gmail.com' && password !== 'AdminPass') {
+        alert('Wrong password')
+    } else {
+        let cpassword = confirm('Do you want to change your password?');
+        if (!cpassword) {
+            alert('You have failed the change')
+        } else {
+            let oldpass = prompt('Enter old password', '');
+            if (oldpass === '' || oldpass === null) {
+                alert('Canceled');
+            } else if (email === 'user@gmail.com' && password !== 'UserPass' || email === 'admin@gmail.com' && password !== 'AdminPass') {
+                alert('Wrong password');
+            } else {
+                let newpass = prompt('Enter new password', '');
+                if (newpass.length < 6) {
+                    alert('It’s too short password. Sorry');
+                } else {
+                    let confirmNewPass = prompt('Confirm your new password', '');
+                    if (newpass === confirmNewPass) {
+                        alert('You have successfully changed your password');
+                    } else {
+                        alert('You wrote the wrong password');
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ FL12_HW7/homework/src/js/task2.js
@@ -0,0 +1,48 @@
+let minNumber = 0;
+let maxNumber = 8;
+let attempts = 3;
+let attempt = 4;
+let maxPrize = 100;
+let prize = 0;
+let changePrize = 2;
+let changeNumber = 4;
+let won = true;
+
+let play = confirm('Do you want to play a game?');
+if (!play) {
+    alert('You did not become a billionaire, but can');
+} else {
+    while (play) {
+    while (won) {
+        let random = Math.floor(Math.random() * (maxNumber + 1));
+        let t = false;
+        for (let i = 1; i > 0; i++) {
+            let userNumber = +prompt(`Choose a roulette pocket number from ${minNumber} to ${maxNumber}
+            \nAttempts left: ${attempt-1}
+            \nTotal price: ${prize}
+            \nPossible prize on current attempt: ${Math.floor(maxPrize / Math.pow(changePrize, i-1))}`)
+            if (userNumber === random) {
+                prize += Math.floor(maxPrize / Math.pow(changePrize, i-1));
+                break;
+            } else {
+                if (i === attempts) {
+                    t = true;
+                    break;
+                }
+            }
+        }
+        if (t) {
+            break;
+        }
+        won = confirm(`Thank you for your participation. Your prize is: ${prize}. Do you want to continue?`);
+        if (won) {
+            maxNumber += changeNumber;
+            maxPrize *= changePrize;
+        } else {
+            break;
+        }
+    }
+    alert(`Thank you for your participation. Your prize is: ${prize}`);
+    play = confirm('Do you want to play again?');
+}
+}
